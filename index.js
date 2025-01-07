eebec96a7d0ac7528b7b12381820123409b46152
charming0524 <oni.adeola@yahoo.com>
2025-01-07 21:33:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// HINTS:
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+
+// 2. Create an express app and set the port number.
+
+const app = express();
+const port = 3000;
+const API_URL = "https://v2.jokeapi.dev/joke";
+
+
+// 3. Use the public folder for static files.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
+    try {
+      const result = await axios.get(API_URL +"/Any");      
+        res.render("index.ejs", {  
+           call : result.data.setup,
+           answer : result.data.delivery,
+            type : result.data.category
+         });
+    } catch (error) {
+      console.log(error.response.data);
+      res.status(500);
+    }
+  });
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+// 6. Listen on your predefined port and start the server.
